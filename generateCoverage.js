2cd71230d725a01d02475f72ac1dc55c93ae3042
Borja <borja.robles@gmail.com>
2024-03-28 17:10:02 +0100

    update

--- generateCoverage.js
+++ generateCoverage.js
@@ -1,22 +1,25 @@
 import { $ } from "bun";
 
-const [fileName, fileContent] = Bun.argv.slice(2);
-const apiKey = process.env.ANTHROPIC_API_KEY;
+const [key, fileContent] = Bun.argv.slice(2);
+// const apiKey = process.env.ANTHROPIC_API_KEY;
 
-const welcome = await $`curl -k https://api.anthropic.com/v1/complete \
---header "x-api-key: ${apiKey}" \
+// Asegurarse de que el contenido del archivo se pasa correctamente como una cadena JSON válida.
+const contentEscaped = JSON.stringify(fileContent);
+
+const response = await $`curl -k -X POST https://api.anthropic.com/v1/messages \
+--header "x-api-key: ${key}" \
 --header "anthropic-version: 2023-06-01" \
 --header "content-type: application/json" \
 --data \
 '{
-"model": "claude-3-opus-20240229"
-"system": "Your task is to perform a code review on the provided code snippet, focusing on improvements and suggestions to ensure the use of the latest features and avoidance of deprecated elements. Provide the response in the specified programming language with minimal explanations and without adding comments to the code itself. Only provide code improvements or suggestions as necessary."
-"max_tokens_to_sample": 1024,
-"stream": false,
-"messages": [
-  {"role": "user", "content": "Please review this code ${fileContent} "}
-]
+  "model": "claude-3-opus-20240229",
+  "temperature": 0,
+  "system": "Your task is to perform a code review on the provided code snippet, focusing on improvements and suggestions to ensure the use of the latest features and avoidance of deprecated elements. Provide the response in the specified programming language with minimal explanations and without adding comments to the code itself. Only provide code improvements or suggestions as necessary.",
+  "messages": [
+    {"role": "user", "content": "Code to review: ${contentEscaped}"}
+  ],
+  "max_tokens": 1024
 }'`.text();
 
-const JSONwelcome = JSON.parse(welcome);
-console.log(JSONwelcome);
+const jsonResponse = JSON.parse(response);
+console.log(jsonResponse);
